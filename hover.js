417fb8831cca19fcfaf749df23f25fc236d482e6
kentakawachi <heneijianda9@gmail.com>
2025-09-08 17:02:01 +0900

    Update hover.js

--- hover.js
+++ hover.js
@@ -1,15 +1,49 @@
-const syabon = document.getElementById('syabon');
-syabon.addEventListener('mouseover', function() {
-  syabon.src = 'syabondaman_hover.png';
-});
-syabon.addEventListener('mouseout', function() {
-  syabon.src = 'syabondaman.png';
-});
+// ページの中身が全部読み込まれてから動くようにする
+window.onload = function() {
 
-const toaster = document.getElementById('toaster');
-toaster.addEventListener('mouseover', function() {
-  toaster.src = 'toasterman_hover.png';
-});
-toaster.addEventListener('mouseout', function() {
-  toaster.src = 'toasterman.png';
-});
+  // シャボンダマン
+  let syabon = document.getElementById("syabondaman");
+  syabon.onmouseenter = function() {
+    syabon.src = "syabondaman_hover.png"; // マウスをのせたら切り替え
+  };
+  syabon.onmouseleave = function() {
+    syabon.src = "syabondaman.png"; // マウスをはなしたら元に戻す
+  };
+
+  // トースターまん
+  let toaster = document.getElementById("toasterman");
+  toaster.onmouseenter = function() {
+    toaster.src = "toasterman_hover.png";
+  };
+  toaster.onmouseleave = function() {
+    toaster.src = "toasterman.png";
+  };
+
+  // かまぼこくん
+  let kamaboko = document.getElementById("kamabokokun");
+  kamaboko.onmouseenter = function() {
+    kamaboko.src = "kamabokokun_hover.png";
+  };
+  kamaboko.onmouseleave = function() {
+    kamaboko.src = "kamabokokun.png";
+  };
+
+  // ノリノリのりへい
+  let norihei = document.getElementById("norihei");
+  norihei.onmouseenter = function() {
+    norihei.src = "norihei_hover.png";
+  };
+  norihei.onmouseleave = function() {
+    norihei.src = "norihei.png";
+  };
+
+  // うずまきパンタ
+  let panta = document.getElementById("panta");
+  panta.onmouseenter = function() {
+    panta.src = "panta_hover.png";
+  };
+  panta.onmouseleave = function() {
+    panta.src = "panta.png";
+  };
+
+};
